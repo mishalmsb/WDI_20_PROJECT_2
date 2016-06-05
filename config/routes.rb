05600d3f87b07e5b2a51262938e4586781e5c938
Mishal Shashikant <mishalmsb@gmail.com>
2016-06-05 21:40:23 +0100

    install devise and  migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :bets
   resources :games
   resources :users
+
+  root 'users#show'
+  get  'users/:id', to: "users#show"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
